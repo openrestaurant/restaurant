02784ae475ee4b42028f16100277ae5a9baf711c
Arshad Chummun <arshad@chummun.com>
2014-08-05 14:47:50 +0400

    Updated update_remotes script to handle profile as well

--- update_remotes.sh
+++ update_remotes.sh
@@ -7,6 +7,24 @@ if [ -z "$TARGET" ]; then
   TARGET=`pwd -P`
 fi
 TARGET=`cd "$TARGET"; pwd -P`
+echo "Updating git remotes for profile..."
+echo ""
+echo ""
+echo "----------------------------------"
+echo "Profile: Restaurant"
+echo "----------------------------------"
+if ! (git remote | grep drupal -q); then
+  # Rename origin to drupal
+  git remote rename origin drupal
+  git remote set-url drupal arshad@git.drupal.org:project/restaurant.git
+fi
+if ! (git remote | grep github -q); then
+  # Add Github remote
+  git remote add --track 7.x-1.x github git@github.com:drupalrestaurant/restaurant.git
+fi
+# Pull latest code from Github
+echo "Pulling latest version of profile restaurant from Github..."
+git pull github 7.x-1.x
 
 echo 'Updating git remotes for modules...'
 submodules=( "admin" "base" "blog" "dashboard" "demo" "event" "foursquare" "location" "mail" "menu" "opentable" "pages" "plugins" "reservation" "seo" "slideshow" "theme" "user" "widgets" "yelp")
