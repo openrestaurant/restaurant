5aad4ee05aafb4efc0824c26a79e3b7b6e2ce4a8
Arshad Chummun <arshad@chummun.com>
2014-08-05 14:39:59 +0400

    Added reinstall and update_remotes scripts

---
+++ reinstall.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Configuration variables
+drupal_root="/home/vagrant/www"
+
+# Check if file exists
+cd ${drupal_root}
+
+echo "Dropping all tables"
+`drush sql-connect` -e "show tables" -s | xargs -I "@@" `drush sql-connect` -e "DROP TABLE @@"
+
+echo "Copying settings.php"
+rm -rf sites/default/settings.php
+cp sites/default/default.settings.php settings.php
+
+echo "Creating files directory"
+rm -rf files
+mkdir files
+chmod -R 777 settings.php files
