2ae472974c482568d92003e5c04fc17fbbe717e0
Arshad Chummun <arshad@chummun.com>
2015-09-20 17:08:46 +0400

    Update location of site

--- scripts/db_reset.sh
+++ scripts/db_reset.sh
@@ -11,8 +11,10 @@ echo ""
 echo "----------------------------------"
 echo "Recreating settings.php in sites/default..."
 echo "----------------------------------"
-cd ../../../
+cd /home/vagrant/www
 chmod 755 sites/default
 cd sites/default
+rm -rf settings.php files
 cp default.settings.php settings.php
-chmod 777 settings.php
+mkdir files
+chmod -R 777 settings.php files
