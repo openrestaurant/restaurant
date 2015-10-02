8ac55cb3a98fdaac814910a66d81925943f36632
Arshad Chummun <arshad@chummun.com>
2015-10-02 20:34:56 +0400

    Update rebuild script

--- scripts/rebuild.sh
+++ scripts/rebuild.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 # Script to rebuild the Restaurant installation profile
-# This command expects to be run within the Drupal root directory.
 # To use this command you must have `drush make` and `git` installed.
 
 echo '=========================='
@@ -8,13 +7,14 @@ echo '    Open Restaurant     '
 echo '=========================='
 
 echo "\nThis command can be used to rebuild the installation profile.\n"
+echo "\nRun this script from Drupal root.\n"
 echo "  [1] Rebuild profile in place in development mode from Github\n"
 echo "Selection: \c"
 read SELECTION
 
 if [ $SELECTION = "1" ]; then
-  echo "Building Drupal Restaurant install profile from Github..."
-  drush make -y --working-copy --no-gitinfofile --contrib-destination=profiles/restaurant profiles/restaurant/github.make
+  echo "Building Open Restaurant install profile from Github..."
+  drush make -y --working-copy --no-gitinfofile --contrib-destination=profiles/restaurant profiles/restaurant/build-restaurant-github.make
 else
  echo "Invalid selection."
 fi
