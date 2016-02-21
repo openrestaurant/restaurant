a99b148756d43a23933960d7771fd1e44e81fbfb
Arshad Chummun <arshad@chummun.com>
2016-02-21 17:45:03 +0400

    Always build profile from drupal.org

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,23 +1,24 @@
 #!/bin/sh
 # Script to build the Restaurant installation profile
+# Run from the Drupal root.
 # To use this command you must have `drush make` and `git` installed.
 
 echo '========================================================================='
 echo '    Open Restaurant - www.open.restaurant                                '
 echo '========================================================================='
 
-echo "\nThis command can be used to build the installation profile."
-echo "\nRun this script from Drupal root."
-echo "  [1] Build profile in place in development mode from Github"
-echo "  [2] Build profile in place in development mode from Drupal"
+echo "\nUse this command to build the Open Restaurant profile."
+echo "\nRun this script from Drupal root.\n"
+echo "  [1] Build profile in place in development mode from drupal.org"
+echo "  [2] Build profile in place in release mode from drupal.org"
 echo "\nSelection: \c"
 read SELECTION
 
 if [ $SELECTION = "1" ]; then
-  echo "Building Open Restaurant install profile from Github..."
-  drush make -y --working-copy --no-gitinfofile --no-cache --contrib-destination=profiles/restaurant profiles/restaurant/build-restaurant-github.make
+  echo "Building Open Restaurant development profile from drupal.org..."
+  drush make -y --working-copy --no-gitinfofile --no-cache --contrib-destination=profiles/restaurant profiles/restaurant/build-restaurant-dev.make
 elif [ $SELECTION = "2" ]; then
-  echo "Building Open Restaurant install profile from Drupal..."
+  echo "Building Open Restaurant release profile from drupal.org..."
   drush make -y --working-copy --no-gitinfofile --no-cache --contrib-destination=profiles/restaurant profiles/restaurant/build-restaurant.make
 else
  echo "Invalid selection."
